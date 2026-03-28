a8a9e7c5f407d3a7c6113c175d60a5455025a180
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-03-28 17:36:13 +0530

    add llm response representration

---
+++ src/dto/llm_schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+from typing import List
+
+
+class Chunk(BaseModel):
+    headline: str = Field(
+        description="A brief heading for this chunk, typically a few words, that is most likely to be surfaced in a query"
+    )
+    summary: str = Field(
+        description="A few sentences summarizing the content of this chunk to answer common questions"
+    )
+    original_text: str = Field(
+        description="The original text of this chunk from the provided document, exactly as is, not changed in any way"
+    )
+
+
+class Chunks(BaseModel):
+    chunks: List[Chunk]
