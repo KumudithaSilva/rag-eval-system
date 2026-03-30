d6e1633a2b3797bfea140abac6083cc7c1dfa543
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-03-30 19:21:48 +0530

    add knowledge base service and RAG pipeline creation to RagEvalContainer

--- src/container/rag_container.py
+++ src/container/rag_container.py
@@ -1,8 +1,13 @@
+from components.knowledge_base_generation import KnowledgeBaseGenerationService
 from components.mongo_connection import MongoConnectionService
+from components.rag_pipeline import RAGPipeline
+from factories.chunking_factory import ChunkingFactory
+from infrastructure.infra.file_extractor import FileExtractor
 from interfaces.infra.i_api_key_provider import IApiKeyProvider
 from interfaces.infra.i_env_loader import IEnvLoader
 from infrastructure.infra.mongo_url_provider import MongoUrlProvider
 from infrastructure.infra.env_loader import DotEnvLoader
+from interfaces.infra.i_file_extractor import IFileExtractor
 
 
 class RagEvalContainer:
@@ -32,3 +37,34 @@ class RagEvalContainer:
             key_provider = MongoUrlProvider(env_loader)
 
         return MongoConnectionService(key_provider)
+
+    def knowledge_base_service(self, file_extractor: IFileExtractor | None = None):
+        """
+        Create and return a knowledge base generation service.
+
+        Args:
+            file_extractor (IFileExtractor, optional): File extractor instance.
+
+        Returns:
+            KnowledgeBaseGenerationService: Knowledge base generation service instance.
+        """
+        if file_extractor is None:
+            file_extractor = FileExtractor()
+
+        return KnowledgeBaseGenerationService(file_extractor)
+
+    def create_pipeline(self, document_config: dict, path: str):
+        """
+        Create and return a RAG pipeline instance.
+
+        Args:
+            document_data (dict): Document data containing chunking configuration.
+            path (str): Path to the documents.
+
+        Returns:
+            RAGPipeline: RAG pipeline instance.
+        """
+
+        chunking = ChunkingFactory.create(document_config["chunking"], path)
+
+        return RAGPipeline(chunking)
