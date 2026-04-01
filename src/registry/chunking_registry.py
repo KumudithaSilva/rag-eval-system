2b41141a363556dc15e25b1f5b02775449c4ae25
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-04-01 18:12:09 +0530

    add dynamic registry for factories

--- src/registry/chunking_registry.py
+++ src/registry/chunking_registry.py
@@ -1,11 +1,29 @@
-from providers.default_chunking_factory import DefaultChunkingFactory
-from providers.llm_chunking_factory import LLMChunkingFactory
+from interfaces.infra.i_chunking_factory import IChunkingFactory
 
-"""
-Registry for chunking strategies.
-"""
 
-CHUNKING_REGISTRY = {
-    "Default Chunking": DefaultChunkingFactory(),
-    "LLM Chunking": LLMChunkingFactory(),
-}
+class FactoryRegistry:
+    """
+    Dynamic registry for factories with dependencies injected at runtime.
+    """
+
+    def __init__(self):
+        self._registry = {}
+
+    def register(self, chunk_type: str, factory: IChunkingFactory):
+        """
+        Register a factory instance for a chunking type.
+
+        args:
+            chunk_type (str): The type of chunking.
+            factory (IChunkingFactory): An instance of a factory that implements IChunkingFactory.
+
+        returns:
+            factory (IChunkingFactory): The registered factory instance for the specified chunk type.
+        """
+        self._registry[chunk_type] = factory
+
+    def get(self, chunk_type: str) -> IChunkingFactory:
+        factory = self._registry.get(chunk_type)
+        if not factory:
+            raise ValueError(f"No factory registered for type: {chunk_type}")
+        return factory
