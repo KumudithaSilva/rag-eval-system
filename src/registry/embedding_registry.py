cdafb3d822e40c93f9882e0722bcbdae24556f50
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-04-01 19:16:38 +0530

    add dynamic registry for embeddings

--- src/registry/embedding_registry.py
+++ src/registry/embedding_registry.py
@@ -1,11 +1,29 @@
-from providers.huggingface_emb_factory import HuggingFaceEmbeddingFactory
-from providers.openai_emb_factory import OpenAIEmbeddingFactory
+from interfaces.embedding.i_embedding import IEmbeddingModel
 
-"""
-Registry for embedding models.
-"""
 
-EMBEDDING_REGISTRY = {
-    "HuggingFace": HuggingFaceEmbeddingFactory(),
-    "OpenAI": OpenAIEmbeddingFactory(),
-}
+class EmbeddingFactoryRegistry:
+    """
+    Dynamic registry for factories with dependencies injected at runtime.
+    """
+
+    def __init__(self):
+        self._registry = {}
+
+    def register(self, emb_type: str, factory: IEmbeddingModel):
+        """
+        Register a factory instance for embedding type.
+
+        args:
+            emb_type (str): The type of embedding.
+            factory (IEmbeddingModel): The factory instance to register.
+
+        returns:
+            factory (IEmbeddingModel): The registered factory instance.
+        """
+        self._registry[emb_type] = factory
+
+    def get(self, emb_type: str) -> IEmbeddingModel:
+        factory = self._registry.get(emb_type)
+        if not factory:
+            raise ValueError(f"No factory registered for embedding type: {emb_type}")
+        return factory
