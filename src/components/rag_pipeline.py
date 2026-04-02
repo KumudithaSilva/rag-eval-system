0ea9db0f5716baac65beeeaaef8843414f1cb46c
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-04-02 18:34:39 +0530

    updated rag pipeline with workflows

--- src/components/rag_pipeline.py
+++ src/components/rag_pipeline.py
@@ -1,33 +1,32 @@
 from interfaces.chunking.i_chunking_strategy import IChunkingStrategy
 from interfaces.embedding.i_embedding import IEmbeddingModel
+from interfaces.infra.i_pipeline import IPipelineStep
 from interfaces.infra.i_rag_pipeline import IRagPipeline
 from interfaces.infra.i_vector_store import IVectorStoreService
 
 
 class RAGPipeline(IRagPipeline):
-    """Pipeline for processing documents."""
+    """Pipeline for processing workflow."""
 
-    def __init__(
-        self,
-        chunking_strategy: IChunkingStrategy,
-        vector_store_service: IVectorStoreService,
-    ):
+    def __init__(self, steps: list[IPipelineStep]):
         """
-        Initialize RAGPipeline with a chunking strategy and embeddings.
+        Initialize RAGPipeline with procedure workflow.
 
         Args:
-            chunking_strategy (IChunkingStrategy): The strategy to use for chunking documents.
-            vector_store_service (IVectorStoreService): The vector store service to use for storing and retrieving vectors.
+            steps (list[IPipelineStep]): The list of procedures.
         """
-        self.chunking = chunking_strategy
-        self.vector_store_service = vector_store_service
+        self.steps = steps
 
-    def process(self):
-        """Process the document using the chunking strategy."""
-        chunks = self.chunking.chunk()
+    def process(self, input_data: dict = None):
+        """
+        Process the rag pipeline.
 
-        vector_store_service = self.vector_store_service.create(chunks=chunks)
-        count = vector_store_service._collection.count()
+        Args:
+            input_data (dict) : Input data for the pipeline step.
+        """
+        data = input_data or {}
 
-        print(f"Vector Store Stored documents count : {count}")
-        return {"count": count}
+        for step in self.steps:
+            data = step.run(data)
+
+        return data
