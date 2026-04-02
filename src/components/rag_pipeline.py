f35e1226e807bc957d50f8a78dd76da3b1ccc63e
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-04-02 16:47:02 +0530

    updated RAGPipeline

--- src/components/rag_pipeline.py
+++ src/components/rag_pipeline.py
@@ -1,29 +1,33 @@
 from interfaces.chunking.i_chunking_strategy import IChunkingStrategy
 from interfaces.embedding.i_embedding import IEmbeddingModel
 from interfaces.infra.i_rag_pipeline import IRagPipeline
+from interfaces.infra.i_vector_store import IVectorStoreService
 
 
 class RAGPipeline(IRagPipeline):
     """Pipeline for processing documents."""
 
     def __init__(
-        self, chunking_strategy: IChunkingStrategy, embedding_model: IEmbeddingModel
+        self,
+        chunking_strategy: IChunkingStrategy,
+        vector_store_service: IVectorStoreService,
     ):
         """
         Initialize RAGPipeline with a chunking strategy and embeddings.
 
         Args:
             chunking_strategy (IChunkingStrategy): The strategy to use for chunking documents.
-            embedding_model (IEmbeddingModel): The embedding model to use for vectorization.
+            vector_store_service (IVectorStoreService): The vector store service to use for storing and retrieving vectors.
         """
         self.chunking = chunking_strategy
-        self.embedding = embedding_model
+        self.vector_store_service = vector_store_service
 
     def process(self):
         """Process the document using the chunking strategy."""
         chunks = self.chunking.chunk()
-        print(f"Chunks: {chunks}")
 
-        embeddings = self.embedding.get_model()
-        print(f"Embeddings: {embeddings}")
-        return {"chunks": chunks}
+        vector_store_service = self.vector_store_service.create(chunks=chunks)
+        count = vector_store_service._collection.count()
+
+        print(f"Vector Store Stored documents count : {count}")
+        return {"count": count}
