faf0abb0a07b3390881d09cc0a8b03c11798497e
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-04-04 21:38:59 +0530

    updated IEvaluator interface with test_questions and retriver_answers parameters

--- src/interfaces/infra/i_evaluator.py
+++ src/interfaces/infra/i_evaluator.py
@@ -1,6 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import List
 
+from dto.result import Result
+from dto.test_rag_retriever import TestQuestion
+
 
 class IEvaluator(ABC):
     """
@@ -8,9 +11,15 @@ class IEvaluator(ABC):
     """
 
     @abstractmethod
-    def evaluate() -> List[dict]:
+    def evaluate(
+        self, test_questions: List[TestQuestion], retriver_answers: List[List[Result]]
+    ) -> List[dict]:
         """
         Evaluate Metrics for all questions in the test set.
+
+        Args:
+            test_questions (List[TestQuestion]): List of Test questions
+            retriver_answers (List[List[Any]]): Retrieved context documents for questions.
 
         Return:
             List[dict]: Evaluated results for multiple metrics.
