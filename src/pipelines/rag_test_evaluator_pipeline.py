a0742f3120a034b22397a961755fc8c9caddd669
Kumuditha Silva <kumudithasilva66@gmail.com>
2026-04-05 16:22:52 +0530

    updated rag test evaluation with removing IRetriver

--- src/pipelines/rag_test_evaluator_pipeline.py
+++ src/pipelines/rag_test_evaluator_pipeline.py
@@ -12,31 +12,27 @@ class EvaluatorStep(IPipelineStep):
     Pipeline step for rag test evaluation operations.
 
     args:
-        rag_test_retriver (IRetriver): Retriever for fetching relevant context documents.
-        rag_tests (List[TestQuestion]): Loaded RAG test questions.
         retrieval_evaluator (IEvaluator): Evaluator for RAG test questions.
+        rag_tests (List[TestQuestion]): Loaded RAG test questions.
     """
 
     def __init__(
         self,
-        rag_test_retriver: IRetriver,
-        rag_tests: List[TestQuestion],
         retrieval_evaluator: IEvaluator,
+        rag_tests: List[TestQuestion],
         logger=None,
     ):
         """
         Initialize ChunkingStep with a chunking strategy.
 
         Args:
-            rag_test_retriver (IRetriver): Retriever for fetching relevant context documents.
-            rag_tests (List[TestQuestion]): Loaded RAG test questions.
             retrieval_evaluator (IEvaluator): Evaluator for RAG test questions.
+            rag_tests (List[TestQuestion]): Loaded RAG test questions.
             logger (Logger, optional): A logger instance. If None, a default
                 logger is created using the class name.
         """
-        self.rag_test_retriver = rag_test_retriver
-        self.rag_tests = rag_tests
         self.retrieval_evaluator = retrieval_evaluator
+        self.rag_tests = rag_tests
         self.logger = logger or Logger(self.__class__.__name__)
 
     def run(self, data: dict):
@@ -46,9 +42,8 @@ class EvaluatorStep(IPipelineStep):
         args:
             data (dict): Store the results of the rag test retriever.
         """
-        retriver = self.rag_test_retriver.fetch_contexts(self.rag_tests)
         evaluator = self.retrieval_evaluator.evaluate(
-            test_questions=self.rag_tests, retriver_answers=retriver
+            test_questions=self.rag_tests, retriver_answers=data["retriver"]
         )
 
         data["evaluator"] = evaluator
